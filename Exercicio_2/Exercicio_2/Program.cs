839abde78b3dfcfa969519420378cb999fae8913
Wesley <36734567+wesleylopes13@users.noreply.github.com>
2019-03-26 23:31:56 -0300

    Exercícios de lógica de programação em C#
    Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o
    código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago.

---
+++ Exercicio_2/Exercicio_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+               casas decimais conforme exemplos.
+               Fórmula: area = π.raio²
+               Considere o valor de π = 3.14159
+               */
+
+            // Declarando variaveis
+            double raio, area = 0;
+                        
+            Console.Write("Informe o raio do círculo: ");
+            raio = Double.Parse(Console.ReadLine());
+
+            // Calculando a área do círculo
+            area = (raio * raio) * 3.14159;
+
+            // Exibindo resultado
+            Console.Write("\nSegue a fórmula: " + raio +"² x 3.14159 = "+ area);
+            Console.ReadLine();
+        }
+    }
+}
